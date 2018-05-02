007d642a06fe010e8fc5e59703132d3129cb4616
anayarojo <raulmartin_04@hotmail.com>
2018-05-02 09:42:52 -0700

    Nuget package

---
+++ AnayaRojo.Tools/Extensions/DataBase/SqlDataReaderExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace AnayaRojo.Tools.Extensions.DataBase
+{
+    public static class SqlDataReaderExtension
+    {
+        public static T Get<T>(this SqlDataReader pObjRecordset, string pStrField) where T : IConvertible
+        {
+            return GetValue<T>(pObjRecordset[pStrField]);
+        }
+
+        public static T Get<T>(this SqlDataReader pObjRecordset, int pIntIndex) where T : IConvertible
+        {
+            return GetValue<T>(pObjRecordset[pIntIndex]);
+        }
+
+        private static T GetValue<T>(object pObjValue) where T : IConvertible
+        {
+            T lUknResultValue = default(T);
+            var lUnkTempValue = pObjValue.ToString();
+
+            if (typeof(T).IsEnum)
+            {
+                lUknResultValue = (T)Enum.Parse(typeof(T), lUnkTempValue, true);
+            }
+            else
+            {
+                lUknResultValue = (T)Convert.ChangeType(lUnkTempValue, typeof(T));
+            }
+
+            return lUknResultValue;
+        }
+    }
+}
